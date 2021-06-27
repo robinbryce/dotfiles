a1d0ea331fbdbbd3acbd3ef4cace3547eace7f1e
Robin Bryce <robin.bryce@jitsuin.com>
2021-06-27 20:42:55 +0100

    updates

---
+++ bin/bootstrap-macos.sh
@@ -0,0 +1,7 @@
+brew install pyenv
+brew install task
+
+CFLAGS="-I$(brew --prefix readline)/include -I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" \
+LDFLAGS="-L$(brew --prefix readline)/lib -L$(brew --prefix openssl)/lib" \
+PYTHON_CONFIGURE_OPTS=--enable-unicode=ucs2 \
+pyenv install -v 3.6.8
